dfd03a2de098b10445c6f64459287e76430b7284
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-05 11:09:59 -0300

    Lista 03 -Questões 15 e 16

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio03/Questao17.java
@@ -0,0 +1,17 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio03;
+
+import java.util.Scanner;
+
+public class Questao17 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Entre com um número:");
+        int numero = scan.nextInt();
+        scan.close();
+
+        
+    }
+}
