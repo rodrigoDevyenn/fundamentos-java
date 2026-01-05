677cc9fe6699924e869ee091ccc5c8185acf9eed
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-04 22:05:17 -0300

    Lista 03 - Questão 01 a 14

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio03/Questao06.java
@@ -0,0 +1,14 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio03;
+
+public class Questao06 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 20; i++)
+            System.out.println(i);
+        
+        for (int i = 1; i <= 20; i++)
+            System.out.print(i + " ");
+        
+    }
+}
---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio03/Questao12.java
@@ -0,0 +1,42 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio03;
+
+import java.util.Scanner;
+
+public class Questao12 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com o número que deseja ver a tabuada:");
+        int numero = scan.nextInt();
+
+        System.out.println("Entre com a operação da tabuada(+, -, *, /):");
+        String operacao = scan.next();
+
+        scan.close();
+
+        switch (operacao) {
+            case "+":
+                System.out.println("Tabuada de " + numero + ":");
+                for (int i = 1; i <= 10; i++){
+                    System.out.println(numero + " + " + i + " = " + (numero + i));
+                }break;
+            case "-":
+                System.out.println("Tabuada de " + numero + ":");
+                for (int i = 1; i <= 10; i++){
+                    System.out.println(numero + " - " + i + " = " + (numero - i));
+                }break;
+            case "*":
+                System.out.println("Tabuada de " + numero + ":");
+                for (int i = 1; i <= 10; i++){
+                    System.out.println(numero + " x " + i + " = " + (numero * i));
+                }break;
+            case "/":
+                System.out.println("Tabuada de " + numero + ":");
+                for (int i = 1; i <= 10; i++){
+                    System.out.println(numero + " / " + i + " = " + ((double)numero / i));
+                }break;
+        }  
+    }
+}
