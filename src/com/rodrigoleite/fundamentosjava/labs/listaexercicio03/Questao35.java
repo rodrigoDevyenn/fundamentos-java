67dc2e55c73bb71e6407b146d61fb66d1267a3a8
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-06 10:08:48 -0300

    Lista 03 - Questões 33 a 36

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio03/Questao35.java
@@ -0,0 +1,31 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio03;
+
+import java.util.Scanner;
+
+public class Questao35 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        double resultado = 0;
+        String registro = "";
+        
+
+        System.out.println("Entre com o n da sequência:");
+        int n = scan.nextInt();
+        scan.close();
+
+        for (int i = 1, j = 1; i <= n; i++, j += 2){
+
+            if (i == n){
+                registro += i + "/" + j + " = ";
+            } else {
+                registro += i + "/" + j + " + ";
+            }
+            resultado += (double) i / j;
+
+        }
+        System.out.print(registro);
+        System.out.print(resultado);
+    }
+}
