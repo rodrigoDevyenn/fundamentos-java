faf748c4adec995aebb86a77f03133ae7685ecc9
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-10 15:22:27 -0300

    Aula 21 - ForEach

---
+++ src/com/rodrigoleite/fundamentosjava/aula21/Foreach.java
@@ -0,0 +1,51 @@
+package com.rodrigoleite.fundamentosjava.aula21;
+
+import java.util.Random;
+
+public class Foreach {
+
+    public static void main(String[] args) {
+        
+        int[] notas = new int[10];
+        Random numeroAleatorio = new Random();
+
+        //Setando os valores
+        for (int i = 0; i < notas.length; i++){
+            notas[i] = numeroAleatorio.nextInt(10);
+        }
+        //Imprimindo
+        for (int i = 0; i < notas.length; i++){
+            System.out.println(notas[i]);
+        }
+
+        System.out.println("Usando o Foreach:");
+        for (int nota : notas){
+            System.out.println(nota);
+        }
+
+        //Como funciona para matrizes?
+        double[][] notasAlunos = new double[3][4];
+        
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 8;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 8;
+        notasAlunos[1][2] = 7;
+        notasAlunos[1][3] = 9;
+
+        notasAlunos[2][0] = 8;
+        notasAlunos[2][1] = 9;
+        notasAlunos[2][2] = 10;
+        notasAlunos[2][3] = 7;
+
+        for (double[] notasAluno : notasAlunos){
+            for (double nota : notasAluno){
+                System.out.print(nota + " ");
+            }
+            System.out.println();
+        }
+    }
+}
