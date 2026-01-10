3e347a3c70ab001b834f186b122cc17dd42715be
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-10 16:03:24 -0300

    Aula 22 - Debug

---
+++ src/com/rodrigoleite/fundamentosjava/aula22/Debug.java
@@ -0,0 +1,13 @@
+package com.rodrigoleite.fundamentosjava.aula22;
+
+public class Debug {
+
+    public static void main(String[] args) {
+        
+        int[] notas = new int[3];
+
+        for (int i = 0; i < 4; i++){
+            notas[i] = i;
+        }
+    }
+}
