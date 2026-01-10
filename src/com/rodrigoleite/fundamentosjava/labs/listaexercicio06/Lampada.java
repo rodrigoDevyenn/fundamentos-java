1b5635d0193fb856a464334ea5dbd66c0294caf2
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-10 18:25:11 -0300

    Lista 06 - Questão  01 com classe Lampada

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio06/Lampada.java
@@ -0,0 +1,14 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio06;
+
+public class Lampada {
+
+    String tipo;
+    String marca;
+    String corLuz;
+    String tensao;
+    double potencia;
+    int vidaUtil;
+    double preco;
+    int garantiaMeses;
+    
+}
