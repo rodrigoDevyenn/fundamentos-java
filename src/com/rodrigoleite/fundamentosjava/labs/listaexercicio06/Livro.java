2637c1d940deba61f6480aa219c546e1c311c43f
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-10 18:40:16 -0300

    Lista 06 - Questão 02 com classe Livro

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio06/Livro.java
@@ -0,0 +1,13 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio06;
+
+public class Livro {
+
+    String titulo;
+    String autor;
+    String editora;
+    int qtdPaginas;
+    String idioma;
+    int faixaEtaria;
+    String dataPublicacao;
+    String genero;
+}
---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio06/Questao02.java
@@ -0,0 +1,19 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio06;
+
+public class Questao02 {
+
+    public static void main(String[] args) {
+        
+        Livro livro = new Livro();
+
+        livro.titulo = "Verity";
+        livro.autor = "Collen Hover";
+        livro.editora = "Galera";
+        livro.dataPublicacao = "09/03/2020";
+        livro.idioma = "Inglês";
+        livro.genero = "Suspense";
+        livro.faixaEtaria = 18;
+        livro.qtdPaginas = 320;
+        
+    }
+}
