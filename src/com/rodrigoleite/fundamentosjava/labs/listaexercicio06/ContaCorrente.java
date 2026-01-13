fc327b230661925f96dd7baa7021a6321bf83e5b
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-13 10:15:08 -0300

    Lista06 - Questão 05 da classe ContaCorrente

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio06/ContaCorrente.java
@@ -0,0 +1,10 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio06;
+
+public class ContaCorrente {
+
+    int numero;
+    double saldo;
+    boolean status;
+    double limite;
+    
+}
