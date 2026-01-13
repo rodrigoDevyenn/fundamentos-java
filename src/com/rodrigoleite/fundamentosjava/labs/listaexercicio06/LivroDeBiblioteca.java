76ef196554e109870ac8c18ade1f429a87b1e9bc
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-13 10:10:29 -0300

    Lista 06 - Questão 04 da classe LivroDeBiblioteca

---
+++ src/com/rodrigoleite/fundamentosjava/labs/listaexercicio06/LivroDeBiblioteca.java
@@ -0,0 +1,17 @@
+package com.rodrigoleite.fundamentosjava.labs.listaexercicio06;
+
+public class LivroDeBiblioteca {
+
+    String titulo;
+    String autor;
+    String editora;
+    int qtdPaginas;
+    String idioma;
+    int faixaEtaria;
+    String dataPublicacao;
+    String genero;
+    int codigo;
+    int tempoMaxEmprestimo;
+    boolean disponivel;
+
+}
