51d30ed53a45b736285cfae4c018998f86b068d9
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-13 10:57:16 -0300

    Aula 25 A - Metodos Simples

---
+++ src/com/rodrigoleite/fundamentosjava/aula25/Carro.java
@@ -0,0 +1,14 @@
+package com.rodrigoleite.fundamentosjava.aula25;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capacidadeCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia(){
+        System.out.println("A autonomia do carro é: "  + (capacidadeCombustivel * consumoCombustivel) + " km");
+    }
+}
