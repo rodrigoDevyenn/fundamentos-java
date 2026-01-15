edbd35e524cc0c5ed9d51f5b8cce12b74a650dbd
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-15 15:40:19 -0300

    Aula 28 - Pacotes

--- src/com/rodrigoleite/fundamentosjava/aula28/Pacotes.java
+++ src/com/rodrigoleite/fundamentosjava/aula28/Pacotes.java
@@ -1,8 +1,17 @@
 package com.rodrigoleite.fundamentosjava.aula28;
 
+// import com.rodrigoleite.fundamentosjava.aula24.Carro;
+// import com.rodrigoleite.fundamentosjava.aula24.labs.Questao01;
+// import com.rodrigoleite.fundamentosjava.aula24.labs.Questao02;
+// import com.rodrigoleite.fundamentosjava.aula24.labs.Questao03;
+
 public class Pacotes {
 
     public static void main(String[] args) {
         
+        // Carro carro;
+        // Questao01 q1;
+        // Questao02 q2;
+        // Questao03 q3;
     }
 }
