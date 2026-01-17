c96cd8ef7a32e5df22b92ddfc85f8864adf50862
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-17 18:02:54 -0300

    Aula 39 - Herança - Polimorfismo

---
+++ src/com/rodrigoleite/fundamentosjava/aula39/Aluno.java
@@ -0,0 +1,48 @@
+package com.rodrigoleite.fundamentosjava.aula39;
+
+public class Aluno extends Pessoa{
+
+    private String curso;
+    private double[] notas;
+
+    public Aluno() {
+        super();
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String cpf, String curso) {
+        super(nome, endereco, telefone, cpf); // Está herdando o método construtor de Pessoa.
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+    public String obterEtiquetaEndereco(){
+        String s = "Endereço do Aluno: ";
+        s += super.getEndereco();
+        return s;
+    }
+
+    
+}
---
+++ src/com/rodrigoleite/fundamentosjava/aula39/Teste.java
@@ -0,0 +1,19 @@
+package com.rodrigoleite.fundamentosjava.aula39;
+
+public class Teste {
+
+    public static void main(String[] args){
+
+        Pessoa pessoa = new Pessoa();
+        Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();
+
+        pessoa.setEndereco("Rua 1, Numero 1");
+        aluno.setEndereco("Rua 2, Numero 2");
+        professor.setEndereco("Rua 3, Numero 3");
+
+        System.out.println(pessoa.obterEtiquetaEndereco());
+        System.out.println(aluno.obterEtiquetaEndereco());
+        System.out.println(professor.obterEtiquetaEndereco());
+    }
+}
