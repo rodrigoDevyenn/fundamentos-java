9331b7bb066ae22be66ca11b3fb73173b5bc1d5c
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-24 18:43:19 -0300

    Aula 77 - Strings: métodos chatAT, getChars, getBytes, toCharArray

---
+++ src/com/rodrigoleite/fundamentosjava/aula75_84/Aula77.java
@@ -0,0 +1,26 @@
+package com.rodrigoleite.fundamentosjava.aula75_84;
+
+import java.util.Arrays;
+
+public class Aula77 {
+
+    public static void main(String[] args){
+
+        String java = "Java";
+
+        for (int i = 0; i < java.length(); i++){
+            System.out.println(java.charAt(i));
+        }
+
+        char[] jav = new char[3];
+        java.getChars(0, 3, jav, 0);
+        System.out.println(jav);
+
+        byte[] javBytes = new byte[3];
+        java.getBytes(0, 3, javBytes, 0);
+        System.out.println(Arrays.toString(javBytes));
+
+        char[] javaChars = java.toCharArray();
+        System.out.println(javaChars);
+    }
+}
