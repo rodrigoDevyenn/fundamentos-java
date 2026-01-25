9f0ca8fda92216709122379cda8b24a845fe6466
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-25 11:03:09 -0300

    Aula 81 - Strings: Métodos toLowerCase e toUpperCase.

---
+++ src/com/rodrigoleite/fundamentosjava/aula75_84/Aula81.java
@@ -0,0 +1,19 @@
+package com.rodrigoleite.fundamentosjava.aula75_84;
+
+public class Aula81 {
+
+    public static void main(String[] args) {
+        
+        String teste = "Teste";
+
+        /*Método: toLowerCase(): transforma todos os caracteres de um string para minúsculas */
+
+        System.out.println(teste); // Teste
+        System.out.println(teste.toLowerCase()); // teste
+
+        /*Método toUpperCase(): transforma todos os caracteres de uma string para maiúsculas */
+
+        System.out.println(teste.toUpperCase()); // TESTE
+
+    }
+}
